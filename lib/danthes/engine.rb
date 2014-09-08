ddfea174831aad3206721a70a6287ba3d0998060
Alexander Simonov <alex@simonov.me>
2014-09-08 11:57:42 +0300

    Fix rails engine initializer

--- lib/danthes/engine.rb
+++ lib/danthes/engine.rb
@@ -3,8 +3,8 @@ require 'danthes/view_helpers'
 module Danthes
   class Engine < Rails::Engine
     # Loads the danthes.yml file if it exists.
-    initializer 'danthes.config' do
-      path = Rails.root.join('config/danthes.yml')
+    initializer 'danthes.config' do |app|
+      path = app.root.join('config/danthes.yml')
       ::Danthes.load_config(path) if path.exist?
     end
 
